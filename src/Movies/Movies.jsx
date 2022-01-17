1f06b6c0a9eaf52eebb45a6955e8bb4de785e16e
sarahishamsaied <sarahishamsaied@gmail.com>
2022-01-18 00:24:25 +0200

    test3

--- src/Movies/Movies.jsx
+++ src/Movies/Movies.jsx
@@ -2,11 +2,14 @@ import axios from 'axios';
 import { Component, Fragment } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css'
 import * as AiIcons from "react-icons/ai";
+import * as BiIcons from 'react-icons/bi'
 import Rating from 'react-rating';
 import { Link } from 'react-router-dom';
 import 'font-awesome/css/font-awesome.css'
 import Details from '../Details/Details';
 import { Redirect } from 'react-router-dom';
+import style from '../Movies/Movies.module.css'
+// import { withRouter } from 'react-router'
 class Movies extends Component {
     baseUrl = 'https://image.tmdb.org/t/p/w500'
     state = {
@@ -29,30 +32,32 @@ class Movies extends Component {
     addToFavorites = (args)=>{
         this.setState({favorites:this.state.favorites.push(args)})
     }
-    detailsButtonHandler = ()=>{
-        
+    detailsButtonHandler = (id)=>{
+        this.props.history.push("/details/"+id)
     }
     render() {
+        console.log(this.props)
         return <Fragment>
-            {/*  */}
-            <section className='movieHeader col-sm-12'>
-                <div className="movieSectionContent col-sm-11 col-md-4">
-                <h1 className='headerTitle text-white'>Spider-Man: No Way Home</h1>
-                <button className='btn btn-danger fw-bold fs-5 w-50'>Book Now</button>
+            
+            
+            <section className={style.movieHeader+ ' col-sm-12'}>
+                <div className={style.movieSectionContent+" p-5 container col-sm-11 col-md-4"}>
+                <h1 className={style.headerTitle+' text-white mb-3'}>The Meg</h1>
+                <button className='btn btn-danger fw-bold fs-5 w-50 my-5'>Book Now</button>
                 </div>
-                <div className="icon ">
+                <div className={style.icon}>
                 {/* <AiIcons.AiFillPlayCircle className='text-white display-1 playBttn'/> */}
                 </div>
-                <div className="socialMedia d-flex flex-column text-white">
-                   <AiIcons.AiOutlineFacebook className='icon'/>
-                    <AiIcons.AiOutlineInstagram className='icon'/>
-                    <AiIcons.AiOutlineTwitter className='icon'/>
+                <div className={style.socialMedia+" d-flex flex-column text-white"}>
+                   <AiIcons.AiOutlineFacebook className={style.icon}/>
+                    <AiIcons.AiOutlineInstagram className={style.icon}/>
+                    <AiIcons.AiOutlineTwitter className={style.icon}/>
                 </div>
             </section>
-            <div className="movieContainer">
-                <h1 className='text-center title pageTitle my-5'>{this.state.title}</h1>
-                <div className="line"></div>
-                <select className = "filter" name="" id="" onChange={(e)=>{
+            <div className={style.movieContainer}>
+                <h1 className={style.pageTitle+' text-center title  my-5'}> <BiIcons.BiTrendingUp/> {this.state.title}</h1>
+                <div className={style.line}></div>
+                <select className = {style.filter} name="" id="" onChange={(e)=>{
                     if(e.target.value === "tv"){this.getTv()}
                     else{this.getMovies()}
                 }}>
@@ -62,24 +67,26 @@ class Movies extends Component {
                 </select>
             <div className="row p-5">
             {this.state.allMovies.map((value,index)=>{
-                    return <div className="col-md-3 col-sm-12 item my-5">
-                            <div className="img-container"> 
-                            <div className="overlay">
-                                <AiIcons.AiFillPlayCircle className='text-danger playBttn'/>
+                    return <div className={"col-md-3 col-sm-12  my-5 "+style.item}>
+                            <div className={style.imgContainer}> 
+                            <div className={style.overlay} onClick={()=>{
+                                this.detailsButtonHandler(value.id)
+                            }}>
+                                <AiIcons.AiFillPlayCircle className={style. playBttn+' text-danger'}/>
                             </div>  
                             <img src={this.baseUrl+value.poster_path} className='img-fluid' alt="" />
                             </div>
-                        <h1 className=' title col-sm-12 itemTitle'>{value.title}</h1>
-                        <h1 className=' title col-sm-12 itemTitle'>{value.name}</h1>
+                        <h1 className={style.title+' col-sm-12 '+style.itemTitle}>{value.title}</h1>
+                        <h1 className={style.title+' col-sm-12 '+style.itemTitle}>{value.name}</h1>
                         <div className="row">
-                        <h1 className = 'col-sm-12 fw-light text-muted title'>Release Date: {value.release_date}{value.first_air_date}</h1>
-                        <span><h1 htmlFor="" className='text-muted fw-light title'>Rating: <Rating initialRating={value.vote_average/2} readonly emptySymbol="fa fa-star-o fa-2x" fullSymbol="fa fa-star fa-2x" stop={5} className='text-muted rating col-md-7'/>
-                        <AiIcons.AiFillHeart className='heart' onClick={()=>{
+                        <h1 className = {'col-sm-12 fw-light text-muted '+style.title}>Release Date: {value.release_date}{value.first_air_date}</h1>
+                        <span><h1 htmlFor="" className={'text-muted fw-light '+style.title}>Rating: <Rating initialRating={value.vote_average/2} readonly emptySymbol="fa fa-star-o fa-2x" fullSymbol="fa fa-star fa-2x" stop={5} className={'text-muted col-md-7 '+style.rating}/>
+                        <AiIcons.AiFillHeart className={style.heart} onClick={()=>{
                             this.addToFavorites(value)
                             console.log(this.state.favorites)
                         }}/></h1></span>
                         </div>
-                        <Link className='btn btn-danger detailsBttn'  to = {"/details/"+value.id}>Details</Link>  
+                        <Link className={'btn btn-danger '+style.detailsBttn}  to = {"/details/"+value.id}>Details</Link>  
 
                     </div>
                 })}
