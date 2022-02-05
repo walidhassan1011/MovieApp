75caadb10c583b74360a9c91983406f78557fc04
walidhassan1011 <walidhassan009@gmail.com>
2022-02-05 07:19:41 +0200

    final-product

--- src/App.js
+++ src/App.js
@@ -1,7 +1,52 @@
 import "./App.css";
+import * as FaBIcons from "react-icons/fa";
+import * as AiIcons from "react-icons/ai";
+import * as CgIcons from "react-icons/cg";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import React, { Component, Fragment } from "react";
 
-function App() {
-  return <></>;
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
+
+import Movies from "./Movies/Movies";
+import Details from "./Details/Details";
+import Search from "./Search/Search";
+import Fav from "./Favorites/Fav";
+import Home from "./Home/Home";
+import Loading from "./Loading/Loading";
+class App extends Component {
+  state = {
+    allMovies: [],
+    favorites: [],
+  };
+  onAddToFavoritesHandler = (fav) => {
+    this.setState({ favorites: [...fav] });
+    console.log(this.state.favorites);
+  };
+  render() {
+    return (
+      <Fragment>
+        <Router>
+          <Switch>
+            <Route exact path={"/"} component={Home} />
+            <Route exact path={"/home"} component={Home} />
+            <Route path="/movies">
+              <Movies onAddToFavorites={this.onAddToFavoritesHandler} />
+            </Route>
+            <Route path="/details/:cat/:id" component={Details} />
+            <Route path="/search" component={Search} />
+            <Route path="/favorites">
+              <Fav favorites={this.state.favorites} />
+            </Route>
+          </Switch>
+        </Router>
+      </Fragment>
+    );
+  }
 }
-
 export default App;
