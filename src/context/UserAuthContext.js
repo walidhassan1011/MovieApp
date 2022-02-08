c7053d818f7cc7be8d579e749b3ce572c0af7e43
walidhassan1011 <walidhassan009@gmail.com>
2022-02-08 18:51:58 +0200

    upgrades

--- src/context/UserAuthContext.js
+++ src/context/UserAuthContext.js
@@ -19,20 +19,7 @@ export function UserAuthContextProvider({ children }) {
     return createUserWithEmailAndPassword(auth, email, password);
   }
   function logIn(email, password) {
-    setPersistence(auth, browserSessionPersistence)
-      .then(() => {
-        // Existing and future Auth states are now persisted in the current
-        // session only. Closing the window would clear any existing state even
-        // if a user forgets to sign out.
-        // ...
-        // New sign-in will be persisted with session persistence.
-        return signInWithEmailAndPassword(auth, email, password);
-      })
-      .catch((error) => {
-        // Handle Errors here.
-        const errorCode = error.code;
-        const errorMessage = error.message;
-      });
+    return signInWithEmailAndPassword(auth, email, password);
   }
   function googleSignIn() {
     const googleAuthProvider = new GoogleAuthProvider();
